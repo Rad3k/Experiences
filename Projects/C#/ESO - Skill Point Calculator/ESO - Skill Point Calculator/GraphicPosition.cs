4c996af7239d635a92a79d43756fca184b5000dc
Rad3k <Rad3k.T@seznam.cz>
2014-01-23 23:07:26 +0100

    Core/C#: Implement new class for graphic positions and remove all images

---
+++ Projects/C#/ESO - Skill Point Calculator/ESO - Skill Point Calculator/GraphicPosition.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ESO___Skill_Point_Calculator
+{
+    class GraphicPosition
+    {
+        // Location
+        public int[][,] Location;
+
+        // Size
+        public int[][,] Size;
+
+        public GraphicPosition()
+        {
+            Location = new int[][,]
+            {
+                // Menu
+                new int[89, 223],
+                // Class
+                new int[325, 200],
+                // Reset Tree
+                new int[410, 169],
+                new int[615, 169],
+                new int[823, 169]
+            };
+            Size = new int[][,]
+            {
+                // Menu
+                new int[133, 26],
+                // Class
+                new int[619, 484],
+                // Reset Tree
+                new int[28,25]
+            };
+        }
+    }
+}
