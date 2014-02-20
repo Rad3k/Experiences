6713ba3fb1f71c1673754d9a196686439307708f
Rad3k <Rad3k.T@seznam.cz>
2014-02-20 20:44:54 +0100

    Core/C#: Implemented offline database for description spells and remove unneeded references

---
+++ Projects/C#/ESO - Skill Point Calculator/ESO - Skill Point Calculator/Database/Transaction.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SQLite;
+using System.Data;
+using System.Windows.Forms;
+
+namespace ESO___Skill_Point_Calculator.Transactions
+{
+    class Transaction
+    {
+    }
+}
