f20aaa3bebfd05ff518035c9a97ab00a40123540
Rad3k <Rad3k.T@seznam.cz>
2014-02-21 19:04:37 +0100

    Core/C#: Added missing file for popup system

---
+++ Projects/C#/ESO - Skill Point Calculator/ESO - Skill Point Calculator/Popup/descriptionSpells.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+namespace ESO___Skill_Point_Calculator.Popup
+{
+    class descriptionSpells
+    {
+        ToolTip popup = new ToolTip();
+        string description = "test";
+        string range = "15";
+        string radius = "2";
+        string castTime = "Instant";
+        string duration = "5";
+
+        public descriptionSpells(object sender)
+        {
+            popup.OwnerDraw = true;
+            popup.Popup += new PopupEventHandler(this.OnPopup);
+            popup.SetToolTip((Button)sender, description + radius + range + castTime + duration);
+            popup.Draw += description_Draw;
+        }
+
+        private void OnPopup(object sender, PopupEventArgs e)
+        {
+            e.ToolTipSize = new Size(200, 100);
+        }
+
+        private void description_Draw(object sender, DrawToolTipEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            LinearGradientBrush b = new LinearGradientBrush(e.Bounds,
+                Color.BurlyWood, Color.Gray, 45f);
+
+            g.FillRectangle(b, e.Bounds);
+
+            g.DrawRectangle(new Pen(Brushes.Black, 1), new Rectangle(e.Bounds.X, e.Bounds.Y,
+                e.Bounds.Width - 1, e.Bounds.Height - 1));
+
+            g.DrawString(e.ToolTipText, new Font(e.Font, FontStyle.Bold), Brushes.Silver,
+                new PointF(e.Bounds.X + 6, e.Bounds.Y + 6));
+            g.DrawString(e.ToolTipText, new Font(e.Font, FontStyle.Bold), Brushes.Black,
+                new PointF(e.Bounds.X + 5, e.Bounds.Y + 5));
+            b.Dispose();
+        }
+    }
+}
